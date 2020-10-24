674918cf87b8e47f748cbf8af922c8f8b15f9069
Joe Francis <joe@lostapathy.com>
2020-10-24 17:21:43 -0500

    fix mousekeys, clean up

--- keyboards/gh60/satan/keymaps/lostapathy/keymap.c
+++ keyboards/gh60/satan/keymaps/lostapathy/keymap.c
@@ -1,47 +1,67 @@
 #include "satan.h"
 
+// Easier-to-read layout
+#define _______ KC_TRNS
+
 enum unicode_names {
   POO,
   SNEK,
-  IRONY
+  IRONY,
+  EGGP,
+  FLEX
 };
 
 const uint32_t PROGMEM unicode_map[] = {
   [POO] = 0x1F4A9, // 💩
   [SNEK] = 0x1F40D, // 🐍
   [IRONY] = 0x2E2E,  // ⸮
+  [EGGP] = 0x1F346, // 🍆
+  [FLEX] = 0x1F4AA, // 💪
 };
+
+#define C_MTCE MT(MOD_LCTL, KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+  // Base layout is basic 60% QWERTY with a few changes
+  // * capslock is escape/control mod_tap combo
+  // * Backspace is split into backspace+delete
+  // * Left shift is split into layer_up+shift
+  // * Right shift is split into shift+layer_up
+  // * Enter is split into two keys, but both keys are enter
   LAYOUT_all(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,
+    C_MTCE,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,
     MO(1),   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, TO(1),
     KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_LALT, KC_RGUI, TO(1),   KC_RCTL),
 
+  // Second layer
+  // * Function keys on number keys
+  // * WASD are mouse direction
+  // * Space is mouse button1
+  // * alt keys are mouse button 2
+  // * hjkl are arrows to vim standard
+  // * +/- are volume up/down
+  // * Backspace/delete are home/end
   LAYOUT_all(
-    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_VOLD, KC_VOLU, KC_HOME, KC_END,
-    KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    TO(0),   KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, MO(2),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_BTN2,                            KC_BTN1,                            KC_BTN2, KC_TRNS, TO(0),   KC_TRNS),
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_VOLD, KC_VOLU, KC_HOME, KC_END,
+    _______, _______, KC_MS_U, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    TO(0),   KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, _______,
+    _______, MO(2),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, KC_BTN2,                            KC_BTN1,                            KC_BTN2, _______, TO(0),   _______),
 
+  // Unicode/emoji layer
+  // Access this by hitting both halfs of left shift plus the letter
   LAYOUT_all(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END,  KC_TRNS, KC_HOME, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_PGUP, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, X(IRONY), KC_TRNS, X(POO), KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, X(SNEK), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    _______, _______, _______, _______, KC_END,  _______, KC_HOME, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______,
+    _______, _______, _______, X(EGGP), _______, _______, _______, _______, X(IRONY), _______, X(POO), _______, _______, _______,
+    _______, _______, X(SNEK), _______, X(FLEX), _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______,                            _______,                            _______, _______, _______, _______),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  //keyevent_t event = record->event;
-
-  //switch (id) {
-
-  //}
   return MACRO_NONE;
 }
 
